37ff86aa753bb0949528ef80695a12ca91720db9
maie2001 <maedehfanaee@gmail.com>
2022-10-06 18:50:24 +0330

    .

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,43 +1,71 @@
-﻿int answre1, answre2, answre;
+﻿double answre1, answre2, answre;
 char symbol;
 List<int> Answre = new List<int>();
 try
 {
     Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("Please enter your number :");
+    Console.WriteLine(" Please elect the desired symbol");
+    Console.WriteLine("* / + - ^ √");
     Console.ResetColor();
-    Console.WriteLine("Number 1 :");
-    answre1 = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Number 2 :");
-    answre2 = Convert.ToInt32(Console.ReadLine());
+    symbol = Convert.ToChar(Console.ReadLine());
     try
     {
-        Console.WriteLine(" Please elect the desired symbol");
-        Console.WriteLine("* / + -");
-        symbol = Convert.ToChar(Answre);
         if (symbol == '*')
         {
+            Answree();
             answre = answre1 * answre2;
+            Console.WriteLine(answre);
         }
         if (symbol == '/')
         {
+            Answree();
             answre = answre1 / answre2;
+            Console.WriteLine(answre);
         }
         if (symbol == '+')
         {
-            answre = answre1 * answre2;
+            Answree();
+            answre = answre1 + answre2;
+            Console.WriteLine(answre);
         }
         if (symbol == '-')
         {
-            answre  = answre1 * answre2;
+            Answree();
+            answre = answre1 - answre2;
+            Console.WriteLine(answre);
         }
-        Answre.Add(answre);
+        if (symbol == '^')
+        {
+            Answree();
+            answre = (int)Math.Pow(answre1, answre2);
+            Console.WriteLine(answre);
+        }
+        if (symbol == '√')
+        {
+            Console.WriteLine("Please enter your number :");
+            answre1 = Convert.ToInt32(Console.ReadLine());
+            answre = (int)Math.Sqrt(answre1);
+            Console.WriteLine(answre);
+        }
     }
-    catch (Exception) { }
+    catch (Exception)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("Wrong input(Just number!)");
+        Console.ResetColor();
+    }
 }
 catch (Exception)
 {
     Console.ForegroundColor = ConsoleColor.DarkRed;
-    Console.WriteLine("Wrong input(Just Number!)");
+    Console.WriteLine("Wrong input(Just symbol!)");
     Console.ResetColor();
+}
+void Answree()
+{
+    Console.WriteLine("Please enter your number :");
+    Console.WriteLine("Number 1 :");
+    answre1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Number 2 :");
+    answre2 = Convert.ToInt32(Console.ReadLine());
 }
