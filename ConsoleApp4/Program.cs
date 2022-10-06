6fbb89e5cd41a0f61fe0264a3681b2753b6050b1
maie2001 <maedehfanaee@gmail.com>
2022-10-06 19:28:21 +0330

    .

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,65 +1,110 @@
 ﻿double answre1, answre2, answre;
 char symbol;
-List<int> Answre = new List<int>();
-try
+List<double> Answre = new List<double>();
+Ask();
+void calculator()
 {
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine(" Please elect the desired symbol");
-    Console.WriteLine("* / + - ^ √");
-    Console.ResetColor();
-    symbol = Convert.ToChar(Console.ReadLine());
     try
     {
-        if (symbol == '*')
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(" Please elect the desired symbol");
+        Console.WriteLine("* / + - ^ √");
+        Console.ResetColor();
+        symbol = Convert.ToChar(Console.ReadLine());
+        try
         {
-            Answree();
-            answre = answre1 * answre2;
-            Console.WriteLine(answre);
+            if (symbol == '*')
+            {
+                Answree();
+                answre = answre1 * answre2;
+                Console.WriteLine(answre);
+                Answre.Add(answre);
+            }
+            if (symbol == '/')
+            {
+                Answree();
+                answre = answre1 / answre2;
+                Console.WriteLine(answre);
+                Answre.Add(answre);
+            }
+            if (symbol == '+')
+            {
+                Answree();
+                answre = answre1 + answre2;
+                Console.WriteLine(answre);
+                Answre.Add(answre);
+            }
+            if (symbol == '-')
+            {
+                Answree();
+                answre = answre1 - answre2;
+                Console.WriteLine(answre);
+                Answre.Add(answre);
+            }
+            if (symbol == '^')
+            {
+                Answree();
+                answre = (int)Math.Pow(answre1, answre2);
+                Console.WriteLine(answre);
+                Answre.Add(answre);
+            }
+            if (symbol == '√')
+            {
+                Console.WriteLine("Please enter your number :");
+                answre1 = Convert.ToInt32(Console.ReadLine());
+                answre = (int)Math.Sqrt(answre1);
+                Console.WriteLine(answre);
+                Answre.Add(answre);
+            }
         }
-        if (symbol == '/')
+        catch (Exception)
         {
-            Answree();
-            answre = answre1 / answre2;
-            Console.WriteLine(answre);
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Wrong input(Just number!)");
+            Console.WriteLine("*****************");
+            Console.ResetColor();
         }
-        if (symbol == '+')
-        {
-            Answree();
-            answre = answre1 + answre2;
-            Console.WriteLine(answre);
-        }
-        if (symbol == '-')
-        {
-            Answree();
-            answre = answre1 - answre2;
-            Console.WriteLine(answre);
-        }
-        if (symbol == '^')
-        {
-            Answree();
-            answre = (int)Math.Pow(answre1, answre2);
-            Console.WriteLine(answre);
-        }
-        if (symbol == '√')
-        {
-            Console.WriteLine("Please enter your number :");
-            answre1 = Convert.ToInt32(Console.ReadLine());
-            answre = (int)Math.Sqrt(answre1);
-            Console.WriteLine(answre);
-        }
+        Console.WriteLine("*****************");
+        Ask();
     }
     catch (Exception)
     {
+        Console.Clear();
         Console.ForegroundColor = ConsoleColor.DarkRed;
-        Console.WriteLine("Wrong input(Just number!)");
+        Console.WriteLine("Wrong input(Just symbol!)");
+        Console.WriteLine("*****************");
         Console.ResetColor();
     }
 }
-catch (Exception)
+void Ask()
 {
-    Console.ForegroundColor = ConsoleColor.DarkRed;
-    Console.WriteLine("Wrong input(Just symbol!)");
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine("Enter 1 to use the calculator :");
+    Console.WriteLine("Enter 2 to show hitory :");
+    Console.WriteLine("Or enter any key to logout :");
     Console.ResetColor();
+    answre1 = Convert.ToInt32(Console.ReadLine());
+    if (answre1 == 1)
+    {
+        Console.Clear();
+        calculator();
+    }
+    if (answre1 == 2)
+    {
+        Console.Clear();
+        Console.WriteLine("History :");
+        foreach (double history in Answre)
+        {
+            Console.WriteLine(history);
+        }
+        Console.WriteLine("*****************");
+        Ask();
+    }
+    else
+    {
+        return;
+    }
 }
 void Answree()
 {
